3da768258b1cd3f2f3987337130ecb2e654daffd
Luis Parra <leparrav@gmail.com>
2014-11-10 22:20:40 +0100

    Have to change static files serving

--- PSite_project/settings.py
+++ PSite_project/settings.py
@@ -2,7 +2,7 @@ import os
 
 # Django settings for PSite_project project.
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 SETTINGS_DIR = os.path.dirname(__file__)
 PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
@@ -26,7 +26,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['leparrav.pythonanywhere.com']
+ALLOWED_HOSTS = ['leparrav.pythonanywhere.com','127.0.0.1']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
