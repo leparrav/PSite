d1869f48636fbb9b587aad40846340481d182bfd
Luis Parra <leparrav@gmail.com>
2014-11-11 10:53:29 +0100

    Quoting changed to simpler version

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
 from blog.models import Quote
-from random import shuffle
+from random import shuffle, randint
 
 def index(request):
 	context = RequestContext(request)
-	quotes = list(Quote.objects.all())
-	shuffle(quotes)
-	context_dict = {'quote': quotes[0]}
+	quote = Quote.objects.get(pk=randint(1,Quote.objects.count()))
+	context_dict = {'quote': quote}
 	return render_to_response('blog/index.html', context_dict, context)
