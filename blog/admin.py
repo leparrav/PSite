4d6d527ef7eb8ec7e61e4999c4acf19c846dc568
Luis Parra <leparrav@gmail.com>
2014-11-11 17:41:47 +0100

    Posts nearly done

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from blog.models import Quote
+from blog.models import Quote, Post, Tag, Category
 
 admin.site.register(Quote)
+admin.site.register(Post)
+admin.site.register(Tag)
+admin.site.register(Category)
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
-from blog.models import Quote
+from blog.models import Quote, Post
 from random import shuffle, randint
 
 def index(request):
 	context = RequestContext(request)
 	quote = Quote.objects.get(pk=randint(1,Quote.objects.count()))
-	context_dict = {'quote': quote}
+	post_list = Post.objects.order_by('-date')[:5]
+	context_dict = {'quote': quote, 'post_list': post_list}
 	return render_to_response('blog/index.html', context_dict, context)
+
+def post(request):
+	context = RequestContext(request)
+	post = Post.objects.get(pk=1)
+	quote = Quote.objects.get(pk=randint(1,Quote.objects.count()))
+	context_dict = {'post': post, 'quote': quote}
+	return render_to_response('blog/post.html', context_dict, context)
