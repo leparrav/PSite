ecbbc098fd49e12df84546c25fbc7494add25cd9
Luis Parra <leparrav@gmail.com>
2014-11-13 16:43:50 +0100

    Added pagination

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, url
 from blog import views
 
 urlpatterns = patterns('',
-	url(r'^$',views.index,name='index'),
+	url(r'(?P<post_page>^\d+)',views.index,name='index'),
 	url(r'post/(?P<post_pk>\d+)',views.post,name='post'),
 	)
--- blog/views.py
+++ blog/views.py
@@ -5,11 +5,33 @@ from blog.forms import CommentForm
 from random import shuffle, randint
 
 
-def index(request):
+def index(request, post_page):
 	context = RequestContext(request)
+	post_page = int(post_page)
 	quote = Quote.objects.get(pk=randint(1,Quote.objects.count()))
-	post_list = Post.objects.order_by('-date')[:7]
-	context_dict = {'quote': quote, 'post_list': post_list}
+	post_len = Post.objects.count()
+
+	# Simple low value
+	if post_page == 1:
+		lower_bound = 0
+	else:
+		lower_bound = 1*post_page 
+	upper_bound = 5*post_page
+
+	if upper_bound >= post_len and lower_bound >= post_len: # I'm losing some post here, fix later
+		no_more = True
+		post_list = Post.objects.order_by('-date')[:5]
+	elif lower_bound <= post_len: # Last posts...
+		post_list = Post.objects.order_by('-date')[lower_bound:]
+		no_more = False
+	else:	
+		post_list = Post.objects.order_by('-date')[lower_bound:upper_bound]
+		no_more = False
+
+	context_dict = {'quote': quote, 
+					'post_list': post_list, 
+					'next': post_page +1,
+					'no_more': no_more}
 	return render_to_response('blog/index.html', context_dict, context)
 
 def post(request, post_pk):
