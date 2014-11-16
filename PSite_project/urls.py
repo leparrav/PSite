2386519bb8b520e896b56ae0d9df603ec0e82d71
Luis Parra <leparrav@gmail.com>
2014-11-16 19:52:59 +0100

    More paths changed

--- PSite_project/urls.py
+++ PSite_project/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^portafolio/',include('portafolio.urls')),
+    url(r'^portafolio/',include('portafolio.urls', namespace="portafolio")),
     url(r'^blog/',include('blog.urls')),
     url(r'^rango/',include('rango.urls')),
     url(r'^$',views.index,name='index'),
